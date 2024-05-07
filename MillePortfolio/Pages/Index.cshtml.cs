b7b98f2db82e4bf4bbeff10ef58c2c937a66d498
Mille Elfver <mille.elfver98@gmail.com>
2024-05-07 18:47:51 +0200

    Added API and published it to azure, added view to portfolio to test

--- MillePortfolio/Pages/Index.cshtml.cs
+++ MillePortfolio/Pages/Index.cshtml.cs
@@ -1,20 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MillePortfolio.Models;
+using Newtonsoft.Json;
 
 namespace MillePortfolio.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly HttpClient _http;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, HttpClient http)
         {
             _logger = logger;
+            _http = http;
+            GitProjects = new List<GitProject>();
         }
 
-        public void OnGet()
-        {
+        public List<GitProject> GitProjects { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            var response = await _http.GetAsync("https://milleprojectapi.azurewebsites.net/GitProject");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                if (!string.IsNullOrEmpty(json))
+                {
+                    GitProjects = JsonConvert.DeserializeObject<List<GitProject>>(json);
+                }
+            }
         }
     }
+
+
 }
