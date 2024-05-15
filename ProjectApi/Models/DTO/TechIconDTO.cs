404e5a6aaf1d98e31954738b392bc1a90c337d56
Mille Elfver <mille.elfver98@gmail.com>
2024-05-15 11:45:14 +0200

    Major update for API, added full CRUD functionality

---
+++ ProjectApi/Models/DTO/TechIconDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectApi.Models.DTO
+{
+    public class TechIconDTO
+    {
+        public string Technology { get; set; } = null!;
+        public string Url { get; set; } = null!;
+    }
+}
--- ProjectApi/Models/TechStack.cs
+++ ProjectApi/Models/TechStack.cs
@@ -1,7 +1,10 @@
-﻿namespace ProjectApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectApi.Models
 {
     public class TechStack
     {
+        [JsonIgnore]
         public int Id { get; set; }
         public TechIcon TechIcon { get; set; } = null!;
     }
