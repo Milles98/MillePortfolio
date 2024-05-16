2e94a124b860abb699f4db3d54bce090470563a4
Mille Elfver <mille.elfver98@gmail.com>
2024-05-16 11:02:11 +0200

    Updated both APIs to follow consume API conventions

--- MillePortfolio/ViewComponents/WeatherViewComponent.cs
+++ MillePortfolio/ViewComponents/WeatherViewComponent.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Caching.Memory;
 using MillePortfolio.Models;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using static System.Net.WebRequestMethods;
 
 namespace MillePortfolio.ViewComponents
 {
@@ -21,7 +23,13 @@ namespace MillePortfolio.ViewComponents
             if (!_cache.TryGetValue("Weather", out WeatherModel weatherData))
             {
                 var client = _clientFactory.CreateClient();
-                var response = await client.GetAsync("http://api.weatherapi.com/v1/current.json?key=abe8320defb74b72adf111532242504&q=Sweden");
+                client.BaseAddress = new Uri("http://api.weatherapi.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("/v1/current.json?key=abe8320defb74b72adf111532242504&q=Sweden");
+
+                //var response = await client.GetAsync("http://api.weatherapi.com/v1/current.json?key=abe8320defb74b72adf111532242504&q=Sweden");
 
                 weatherData = new WeatherModel();
 
