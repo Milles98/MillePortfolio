b7ec2a4f74cb8a4a83366ab49205b3592f42bb2d
Mille Elfver <mille.elfver98@gmail.com>
2024-05-18 09:16:59 +0200

    Updated weather API to more accurately display weather icons, also gave added colorful icons

--- MillePortfolio/Models/WeatherApiResponse.cs
+++ MillePortfolio/Models/WeatherApiResponse.cs
@@ -14,5 +14,14 @@
     public class Current
     {
         public double temp_c { get; set; }
+        public Condition condition { get; set; }
+        public double wind_kph { get; set; }
+        public double precip_mm { get; set; }
+    }
+
+    public class Condition
+    {
+        public string text { get; set; }
+        public string icon { get; set; }
     }
 }
--- MillePortfolio/Models/WeatherModel.cs
+++ MillePortfolio/Models/WeatherModel.cs
@@ -4,5 +4,9 @@
     {
         public string Location { get; set; }
         public string Temperature { get; set; }
+        public string ConditionText { get; set; }
+        public string ConditionIcon { get; set; }
+        public double WindSpeed { get; set; }
+        public double Precipitation { get; set; }
     }
 }
